db48ddad8108e74ae4c111f889285b2aa84eaa3d
Kihyuk Hong <hominot@gmail.com>
2018-09-30 13:49:33 +0900

    add test case for accuracy metric evaluation code

--- metric_learning/metrics/accuracy.py
+++ metric_learning/metrics/accuracy.py
@@ -11,18 +11,30 @@ def cosine_similarity(x, y, axis):
     return -tf.reduce_sum(tf.multiply(x_norm, y_norm), axis=axis)
 
 
+def euclidean_distance(x, y, axis):
+    return tf.norm(x - y, axis=axis)
+
+
+def dot_product(x, y, axis):
+    return -tf.reduce_sum(tf.multiply(x, y), axis=axis),
+
+
+def evaluate_accuracy(func, anchor_embeddings, positive_embeddings, negative_embeddings):
+    p = func(anchor_embeddings, positive_embeddings, axis=1)
+    n = tf.reduce_min(func(negative_embeddings, anchor_embeddings, axis=2), axis=0)
+    return p < n
+
+
 class Accuracy(Metric):
     name = 'accuracy'
 
     metric_functions = {
-        'accuracy:euclidean': lambda x, y, axis: tf.norm(x - y, axis=axis),
+        'accuracy:euclidean': euclidean_distance,
         'accuracy:cosine_similarity': cosine_similarity,
-        'accuracy:dot_product': lambda x, y, axis:
-            -tf.reduce_sum(tf.multiply(x, y), axis=axis),
+        'accuracy:dot_product': dot_product,
     }
 
     def compute_metric(self, model, test_ds):
-
         total = 0.
         success_counts = defaultdict(float)
         for anchor_images, positive_images, negative_images_group in test_ds:
@@ -32,9 +44,8 @@ class Accuracy(Metric):
             positive_embeddings = model(positive_images, training=False)
             negative_embeddings = tf.stack([model(negative_images) for negative_images in negative_images_group])
             for metric, func in self.metric_functions.items():
-                p = func(anchor_embeddings, positive_embeddings, axis=1)
-                n = tf.reduce_min(func(negative_embeddings, anchor_embeddings, axis=2), axis=0)
-                success_counts[metric] += float(sum(tf.cast(p < n, tf.float32)))
+                results = evaluate_accuracy(func, anchor_embeddings, positive_embeddings, negative_embeddings)
+                success_counts[metric] += float(sum(tf.cast(results, tf.float32)))
 
             total += int(anchor_images.shape[0])
 
