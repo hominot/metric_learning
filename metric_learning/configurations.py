efe857c7c4c44f26bae98ce1fe9f510a0cd806e9
Kihyuk Hong <hominot@gmail.com>
2018-09-30 05:10:45 +0000

    clean up configurations

--- metric_learning/configurations.py
+++ metric_learning/configurations.py
@@ -1,4 +1,50 @@
 configs = {
+    'lfw_inception_latent_position_unit': {
+        'dataset': {
+            'name': 'lfw',
+            'image': {
+                'width': 250,
+                'height': 250,
+                'channel': 3,
+            },
+            'train': {
+                'data_directory': '/tmp/research/experiment/lfw/train',
+                'batch_size': 32,
+                'group_size': 2,
+                'num_groups': 8,
+                'min_class_size': 2,
+            },
+            'test': {
+                'data_directory': '/tmp/research/experiment/lfw/test',
+                'num_negative_examples': 5,
+            },
+        },
+        'model': {
+            'name': 'inception',
+            'loss': {
+                'name': 'latent_position',
+                'method': 'distance',
+                'parametrization': 'unit',
+                'alpha': 4.0,
+            },
+            'image': {
+                'width': 250,
+                'height': 250,
+                'channel': 3,
+            },
+        },
+        'metrics': [
+            {
+                'name': 'accuracy',
+                'compute_period': 200,
+                'skip_steps': 10000,
+            },
+        ],
+        'optimizer': {
+            'learning_rate': 0.0005,
+        },
+        'num_epochs': 100,
+    },
     'lfw_inception_latent_position_bias': {
         'dataset': {
             'name': 'lfw',
@@ -10,9 +56,9 @@ configs = {
             'train': {
                 'data_directory': '/tmp/research/experiment/lfw/train',
                 'batch_size': 32,
-                'group_size': 4,
+                'group_size': 2,
                 'num_groups': 8,
-                'min_class_size': 4,
+                'min_class_size': 2,
             },
             'test': {
                 'data_directory': '/tmp/research/experiment/lfw/test',
@@ -36,12 +82,13 @@ configs = {
             {
                 'name': 'accuracy',
                 'compute_period': 200,
+                'skip_steps': 10000,
             },
         ],
         'optimizer': {
-            'learning_rate': 0.001,
+            'learning_rate': 0.0005,
         },
-        'num_epochs': 50,
+        'num_epochs': 100,
     },
     'lfw_inception_npair': {
         'dataset': {
@@ -56,7 +103,7 @@ configs = {
                 'batch_size': 32,
                 'group_size': 2,
                 'num_groups': 8,
-                'min_class_size': 4,
+                'min_class_size': 2,
             },
             'test': {
                 'data_directory': '/tmp/research/experiment/lfw/test',
@@ -79,11 +126,12 @@ configs = {
             {
                 'name': 'accuracy',
                 'compute_period': 200,
+                'skip_steps': 10000,
             },
         ],
         'optimizer': {
-            'learning_rate': 0.0001,
+            'learning_rate': 0.00005,
         },
-        'num_epochs': 50,
+        'num_epochs': 100,
     },
 }
