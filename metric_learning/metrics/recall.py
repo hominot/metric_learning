fix recall metric calculation script

metric_learning/metrics/recall.py
@@ -39,10 +39,10 @@ class Recall(Metric):
                 all_labels += list(test_labels.numpy())
                 test_embeddings = model(test_images, training=False)
                 distances = tf.reduce_sum(
-                    tf.square(embeddings[None] - test_embeddings[:, None]),
+                    tf.square(test_embeddings[None] - embeddings[:, None]),
                     axis=2)
                 distance_blocks.append(distances)
-            tf.concat(distance_blocks, axis=1)
+
             values, indices = tf.nn.top_k(-tf.concat(distance_blocks, axis=1), max(self.conf['k']) + 1)
             top_labels = tf.gather(tf.constant(all_labels), indices)[:, 1:]
             for k in self.conf['k']:
@@ -51,4 +51,5 @@ class Recall(Metric):
             total += int(images.shape[0])
 
         ret = {'recall@{}'.format(k): success / float(total) for k, success in successes.items()}
+        print(ret)
         return ret

metric_learning/run.py
@@ -34,7 +34,7 @@ def train(conf):
         'identification': data_loader.create_identification_test_dataset(
             testing_files, testing_labels).batch(48).prefetch(48),
         'recall': data_loader.create_recall_test_dataset(
-            testing_files, testing_labels).batch(2).prefetch(2),
+            testing_files, testing_labels).batch(48).prefetch(48),
     }
 
     step_counter = tf.train.get_or_create_global_step()