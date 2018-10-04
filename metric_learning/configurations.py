9099e1a5dc11753ea1893edf823083d034d9b7ec
Kihyuk Hong <hominot@gmail.com>
2018-10-04 08:17:04 +0000

    npair configuration update

--- metric_learning/configurations.py
+++ metric_learning/configurations.py
@@ -58,23 +58,28 @@ configs = {
         },
         'num_epochs': 50,
     },
-    'lfw_inception_npair': {
-        'dataset': {
-            'name': 'lfw',
-            'image': {
-                'width': 250,
-                'height': 250,
-                'channel': 3,
+    'stanford_inception_npair': {
+        'image': {
+            'width': 250,
+            'height': 250,
+            'channel': 3,
+            'random_crop': {
+                'width': 224,
+                'height': 224,
+                'n': 8,
             },
+        },
+        'dataset': {
+            'name': 'stanford_online_product',
             'train': {
-                'data_directory': '/tmp/research/experiment/lfw/train',
+                'data_directory': '/tmp/research/experiment/stanford_online_product/train',
                 'batch_size': 32,
                 'group_size': 2,
-                'num_groups': 8,
+                'num_groups': 16,
                 'min_class_size': 2,
             },
             'test': {
-                'data_directory': '/tmp/research/experiment/lfw/test',
+                'data_directory': '/tmp/research/experiment/stanford_online_product/test',
                 'identification': {
                     'num_testcases': 10000,
                     'num_negative_examples': 5,
@@ -89,12 +94,7 @@ configs = {
             'name': 'inception',
             'loss': {
                 'name': 'npair',
-                'n': 8,
-            },
-            'image': {
-                'width': 250,
-                'height': 250,
-                'channel': 3,
+                'n': 16,
             },
             'metric': 'cosine_similarity',
         },
@@ -103,18 +103,13 @@ configs = {
                 'name': 'recall',
                 'compute_period': 200,
                 'k': [1, 2, 4, 8],
-                'batch_size': 64,
-            },
-            {
-                'name': 'accuracy',
-                'compute_period': 200,
-                'batch_size': 64,
+                'batch_size': 48,
             },
         ],
         'optimizer': {
-            'learning_rate': 0.00005,
+            'learning_rate': 0.0001,
         },
-        'num_epochs': 100,
+        'num_epochs': 50,
     },
     'mnist_simple_dense': {
         'image': {
