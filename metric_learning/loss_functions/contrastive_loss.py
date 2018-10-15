36d480fb67b74748c2017fcb6d4f6b04d91652f7
Kihyuk Hong <hominot@gmail.com>
2018-10-15 16:36:24 +0900

    Fix contrastive loss.

--- metric_learning/loss_functions/contrastive_loss.py
+++ metric_learning/loss_functions/contrastive_loss.py
@@ -10,13 +10,13 @@ class ContrastiveLossFunction(LossFunction):
     name = 'contrastive'
 
     def loss(self, embeddings, labels, *args, **kwargs):
-        alpha = self.conf['loss'].get('alpha')
+        alpha = self.conf['loss']['alpha']
         pairwise_distances = upper_triangular_part(pairwise_euclidean_distance_squared(embeddings, embeddings))
         matching_labels_matrix = tf.cast(
             upper_triangular_part(tf.cast(pairwise_matching_matrix(labels, labels), tf.int64)),
             tf.bool)
         positive_distances = tf.boolean_mask(pairwise_distances, matching_labels_matrix)
         negative_distances = tf.boolean_mask(pairwise_distances, ~matching_labels_matrix)
-        loss_value = sum(positive_distances) + sum(tf.maximum(0, alpha - negative_distances))
+        loss_value = (sum(positive_distances) + sum(tf.square(tf.maximum(0, alpha - tf.sqrt(negative_distances))))) / int(pairwise_distances.shape[0])
 
         return loss_value
