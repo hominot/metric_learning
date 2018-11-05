4ad2a73c111af954a49612cae8165c6f8697333a
Kihyuk Hong <hominot@gmail.com>
2018-11-06 02:12:41 +0900

    fix

--- metric_learning/sqs_consumer.py
+++ metric_learning/sqs_consumer.py
@@ -10,13 +10,11 @@ conn_sqs = boto3.resource('sqs')
 if __name__ == '__main__':
     tf.enable_eager_execution()
     queue = conn_sqs.get_queue_by_name(QueueName='experiment-configs')
-    while True:
-        messages = queue.receive_messages(
-            MaxNumberOfMessages=1,
-            WaitTimeSeconds=0)
+    messages = queue.receive_messages(
+        MaxNumberOfMessages=1,
+        WaitTimeSeconds=0)
 
-        for message in messages:
-            conf = json.loads(message.body)
-            message.delete()
-            tf.set_random_seed(1) # hack for resetting gpu memory
-            train(conf)
+    for message in messages:
+        conf = json.loads(message.body)
+        message.delete()
+        train(conf)
