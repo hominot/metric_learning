45331eefbea7f875108179ec8c9b3cb11f31d891
Kihyuk Hong <hominot@gmail.com>
2018-11-10 00:30:42 +0900

    VRF@n metric

--- metric_learning/example_configurations.py
+++ metric_learning/example_configurations.py
@@ -7,7 +7,7 @@ configs = {
         'batch_design': {'name': 'grouped', 'group_size': 2, 'batch_size': 16, 'npair': 2},
         'model': {'name': 'simple_dense', 'dimension': 2},
         'loss': {'name': 'npair', 'alpha': 4, 'importance_sampling': True},
-        'metrics': {'auc': True, 'recall': True, 'nmi': True},
+        'metrics': {'vrf': True, 'vrf_k': [1, 2]},
         'trainer': {'lr_decay_steps': 10},
     }),
 }
