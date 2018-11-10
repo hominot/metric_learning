4094a5d6bf3b50c39e6f541d8e1328e45102ec0e
Kihyuk Hong <hominot@gmail.com>
2018-11-10 18:38:24 +0900

    lifted structure loss

--- metric_learning/example_configurations.py
+++ metric_learning/example_configurations.py
@@ -4,9 +4,9 @@ configs = {
     'mnist_simple_dense': generate_config({
         'image': {},
         'dataset': {'name': 'mnist'},
-        'batch_design': {'name': 'grouped', 'group_size': 2, 'batch_size': 4, 'npair': 2, 'negative_class_mining': True},
+        'batch_design': {'name': 'grouped', 'group_size': 2, 'batch_size': 16},
         'model': {'name': 'simple_dense', 'dimension': 2},
-        'loss': {'name': 'npair', 'alpha': 4, 'importance_sampling': True},
+        'loss': {'name': 'lifted', 'alpha': 1},
         'metrics': {'vrf': True, 'vrf_k': [1, 2]},
         'trainer': {'lr_decay_steps': 10},
     }),
