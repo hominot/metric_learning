0424dfd646d14262e60a0132a4dc61fb634909aa
Kihyuk Hong <hominot@gmail.com>
2018-11-13 10:00:32 +0900

    optimize recall calculation

--- metric_learning/metrics/recall.py
+++ metric_learning/metrics/recall.py
@@ -17,35 +17,23 @@ def count_singletons(all_labels):
 
 def compute_recall(embeddings_list, labels_list, k_list, distance_function):
     successes = defaultdict(float)
-    total = 0.
-    num_singletons = 0
-    data = list(zip(embeddings_list, labels_list))
-    batches = tqdm(
-        data, total=len(embeddings_list), desc='recall', dynamic_ncols=True)
-    for i, (embeddings, labels) in enumerate(batches):
-        all_labels = []
-        distance_blocks = []
-        for j, (test_embeddings, test_labels) in enumerate(data):
-            all_labels += list(test_labels.numpy())
-            pairwise_distances = compute_pairwise_distances(
-                embeddings, test_embeddings, distance_function)
-            if i == j:
-                pairwise_distances = pairwise_distances + \
+    embeddings = tf.concat(embeddings_list, axis=0)
+    labels = tf.concat(labels_list, axis=0)
+    pairwise_distances = compute_pairwise_distances(
+        embeddings, embeddings, distance_function)
+    pairwise_distances = pairwise_distances + \
                          tf.eye(int(pairwise_distances.shape[0])) * 1e6
-            distance_blocks.append(pairwise_distances)
-
-        values, indices = tf.nn.top_k(-tf.concat(distance_blocks, axis=1), max(k_list))
-        top_labels = tf.gather(tf.constant(all_labels, tf.int64), indices)
-        for k in k_list:
-            score = tf.reduce_sum(
-                tf.cast(tf.equal(
-                    tf.transpose(labels[None]),
-                    top_labels[:, 0:k]
-                ), tf.int32), axis=1)
-            successes[k] += int(sum(tf.cast(score >= 1, tf.int32)))
-        total += int(embeddings.shape[0])
-        num_singletons = count_singletons(all_labels)
-    return {k: success / float(total - num_singletons) for k, success in successes.items()}
+    values, indices = tf.nn.top_k(-pairwise_distances, max(k_list))
+    top_labels = tf.gather(tf.constant(labels, tf.int64), indices)
+    for k in k_list:
+        score = tf.reduce_sum(
+        tf.cast(tf.equal(
+            tf.transpose(labels[None]),
+            top_labels[:, 0:k]
+        ), tf.int32), axis=1)
+        successes[k] += int(sum(tf.cast(score >= 1, tf.int32)))
+    num_singletons = count_singletons(labels.numpy().tolist())
+    return {k: success / float(int(labels.shape[0]) - num_singletons) for k, success in successes.items()}
 
 
 class Recall(Metric):
