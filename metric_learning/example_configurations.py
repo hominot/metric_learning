92e98af845085ba5cd7609d4176fa3eb9478badc
EC2 Default User <ec2-user@ip-172-31-44-224.us-west-2.compute.internal>
2019-03-08 12:38:35 +0000

    Enable new_importance_sampling for l1_contrastive loss

--- metric_learning/example_configurations.py
+++ metric_learning/example_configurations.py
@@ -1,14 +1,32 @@
 from util.config import generate_config
 
 configs = {
-    'mnist_simple_dense': generate_config({
+    'cub200_contrastive': generate_config({
         'image': {},
-        'dataset': {'name': 'mnist'},
-        'batch_design': {'name': 'grouped', 'group_size': 2, 'batch_size': 16, 'negative_class_mining': True},
-        'model': {'name': 'simple_dense', 'dimension': 2},
-        'loss': {'name': 'contrastive', 'alpha': 4, 'importance_sampling': True},
-        'metrics': {'vrf': True, 'vrf_k': [1, 2]},
-        'trainer': {'lr_decay_steps': 10},
+        'dataset': {'name': 'cub200'},
+        'batch_design': {'name': 'grouped', 'group_size': 4, 'batch_size': 64, 'num_batches': 100},
+        'model': {'name': 'resnet50', 'dimension': 128},
+        'loss': {'name': 'l1_contrastive', 'alpha': 4.0, 'new_importance_sampling': True, 'l': 512},
+        'metrics': {'recall': True, 'recall_k': [1, 2]},
+        'trainer': {'lr_decay_steps': 100, 'learning_rate': 0.00003, 'num_epochs': 20, 'lr_decay_rate': 0.9},
+    }),
+    'lfw_contrastive': generate_config({
+        'image': {},
+        'dataset': {'name': 'lfw'},
+        'batch_design': {'name': 'grouped', 'group_size': 4, 'batch_size': 64, 'num_batches': 100},
+        'model': {'name': 'resnet50', 'dimension': 128},
+        'loss': {'name': 'l1_contrastive', 'alpha': 8.0, 'new_importance_sampling': True, 'l': 512},
+        'metrics': {'recall': True, 'recall_k': [1, 2]},
+        'trainer': {'lr_decay_steps': 1000, 'learning_rate': 0.00003, 'num_epochs': 200, 'lr_decay_rate': 0.9},
+    }),
+    'lfw_npair': generate_config({
+        'image': {},
+        'dataset': {'name': 'lfw'},
+        'batch_design': {'name': 'grouped', 'group_size': 2, 'batch_size': 64, 'npair': 32},
+        'model': {'name': 'resnet50', 'dimension': 128},
+        'loss': {'name': 'npair', 'lambda': 0.001},
+        'metrics': {'recall': True, 'recall_k': [1, 2]},
+        'trainer': {'lr_decay_steps': 100, 'learning_rate': 0.00003, 'num_epochs': 20, 'lr_decay_rate': 0.9},
     }),
     'cars196_npair': generate_config({
         'image': {},
