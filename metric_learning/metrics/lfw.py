54137447989f4e10970cd1c4fe3463d5f40ae677
Taeyoung Lee <ty91kr@gmail.com>
2019-03-10 15:13:41 +0900

    Compute pairwise distances

--- metric_learning/metrics/lfw.py
+++ metric_learning/metrics/lfw.py
@@ -1,10 +1,25 @@
-from util.registry.metric import Metric
+from __future__ import print_function
 
 import tensorflow as tf
+from tqdm import tqdm
+
+from util.registry.metric import Metric
+from util.tensor_operations import compute_pairwise_distances
+from metric_learning.constants.distance_function import get_distance_function
+
 
 class LFW(Metric):
     name = 'lfw'
 
     def compute_metric(self, model, ds, num_testcases):
         embeddings_list, labels_list = self.get_embeddings(model, ds, num_testcases)
+        distance_function = get_distance_function(self.conf['loss']['distance_function'])
+        data = list(zip(embeddings_list, labels_list))
+        batches = tqdm(
+            data, total=len(embeddings_list), desc='lfw', dynamic_ncols=True)
+        for i, (embeddings, labels) in enumerate(batches):
+            for j, (test_embeddings, test_labels) in enumerate(data):
+                pairwise_distances = compute_pairwise_distances(
+                    embeddings, test_embeddings, distance_function)
+
         return {'lfw': 1.0}
